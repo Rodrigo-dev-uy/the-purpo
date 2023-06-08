c9f392b804f946c659d929ee472a681ca3959b0b
Rodrigo Costa <ro.costa.dev@gmail.com>
2023-06-08 01:12:14 -0300

    first

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,8 @@
 import React from "react";
-import { useEffect, useState } from "react";
+
 function Home() {
   const [scrollPosition, setScrollPosition] = useState(0);
 
-  const handleScroll = () => {
-    const position = window.scrollY;
-    setScrollPosition(position);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return <div>Home</div>;
 }
 
